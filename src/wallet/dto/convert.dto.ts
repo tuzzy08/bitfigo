edb1049d534a2795b04810ab91ad595dfdb57525
Sage <tuzzy08@gmail.com>
2022-03-18 16:30:29 +0100

    Implemented validation

--- src/wallet/dto/convert.dto.ts
+++ src/wallet/dto/convert.dto.ts
@@ -1,18 +1,27 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class ConvertDTO {
   // This annotation populates these properties
   // on the swagger documentation endpoint as it
   // doesn't populate by default
+  @IsNotEmpty()
+  @IsString()
   @ApiProperty()
   userId: string;
 
+  @IsNotEmpty()
+  @IsString()
   @ApiProperty()
   baseToken: string;
 
+  @IsNotEmpty()
+  @IsString()
   @ApiProperty()
   destinationToken: string;
 
+  @IsNotEmpty()
+  @IsString()
   @ApiProperty()
   baseTokenAmount: number;
 }
